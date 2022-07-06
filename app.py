c3729b96d87860e60a35c0fbd8fb2e4f9aa2d0e0
RussellShire <russell.shire@gmail.com>
2022-07-06 11:16:24 +0100

    Added some input cleaning to the menu, checking rating is valid and converting text to titlescript for later GROUP BY SQLite functions

--- app.py
+++ app.py
@@ -20,9 +20,19 @@ def menu():
 
     while (user_input := input(MENU_PROMPT)) != "5":
         if user_input == "1":
-            print("you selected 1")
+            name = input("Enter Coffee Bean name: \n").title()
+            method = input("Enter how you prepared your coffee: \n").title()
+            rating = int(input("How do you score the coffee out of 100?: \n"))
+
+            if rating >100 or rating < 0:
+                rating = int(input("Invalid score, please rate 0-100: \n"))
+
+            database.add_bean(connection, name, method, rating)
         elif user_input == "2":
-            print("you selected 2")
+            beans = database.get_all_beans(connection)
+            # For loop prints each row as a new line (print(beans) prints them in a list)
+            for bean in beans:
+                print(bean)
         elif user_input == "3":
             print("you selected 3")
         elif user_input == "4":
