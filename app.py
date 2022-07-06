ac2f15142eb931e76751240a09f0a3fb73b86da1
RussellShire <russell.shire@gmail.com>
2022-07-06 12:07:24 +0100

    Used SQL to make the database lowercase so GROUP BY functions would work on any input. Made future inputs be lower by default. Made searches of db lower, but returns print titlecase

--- app.py
+++ app.py
@@ -20,11 +20,11 @@ def menu():
 
     while (user_input := input(MENU_PROMPT)) != "5":
         if user_input == "1":
-            name = input("Enter Coffee Bean name: \n").title()
-            method = input("Enter how you prepared your coffee: \n").title()
+            name = input("Enter Coffee Bean name: \n").lower()
+            method = input("Enter how you prepared your coffee: \n").lower()
             rating = int(input("How do you score the coffee out of 100?: \n"))
 
-            if rating >100 or rating < 0:
+            if rating > 100 or rating < 0:
                 rating = int(input("Invalid score, please rate 0-100: \n"))
 
             database.add_bean(connection, name, method, rating)
@@ -32,9 +32,13 @@ def menu():
             beans = database.get_all_beans(connection)
             # For loop prints each row as a new line (print(beans) prints them in a list)
             for bean in beans:
-                print(bean)
+                print(f"{bean[1].title()} ({bean[2].title()}) - {bean[3]}/100")
         elif user_input == "3":
-            print("you selected 3")
+            name = input("Enter coffee bean name:\n").lower()
+
+            beans = database.get_beans_by_name(connection, name)
+            for bean in beans:
+                print(f"{bean[1].title()} ({bean[2].title()}) - {bean[3]}/100")
         elif user_input == "4":
             print("you selected 4")
         else:
--- database.py
+++ database.py
@@ -18,6 +18,10 @@ WHERE name = ?
 ORDER BY rating DESC
 LIMIT 1;"""
 
+LOWERCASE_DATABASE = """
+UPDATE beans
+SET name = LOWER(name), method = lower(method);"""
+
 
 def connect():
     return sqlite3.connect("data.db")
@@ -48,3 +52,13 @@ def get_best_method(connection, name):
     with connection:
         return connection.execute(GET_BEST_METHOD, (name,)).fetchone()
 
+
+# created to make my prevously open text entries uniformly lowercase for GROUP BY funcitonality
+def lowercase_database():
+    connection = connect()
+    with connection:
+        connection.execute(LOWERCASE_DATABASE)
+
+
+# only needed to be ran once on older data that had been added before inputs were formatted to lower
+# lowercase_database()
